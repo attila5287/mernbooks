0b93cabf695438e10cb5303421f1c0fb710051d8
attila5287 <atiturkoz@hotmail.com>
2021-05-23 00:26:54 -0600

    build and dep

---
+++ client/build/precache-manifest.9de47c041a0a20141bcbb168fcbde45f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57423c38c288bea9c7e0e2e321b3880b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0cae6f5829ea57f0fce",
+    "url": "/static/css/2.0326acd9.chunk.css"
+  },
+  {
+    "revision": "8ce4b511767afa85ab2d",
+    "url": "/static/css/main.eba327d7.chunk.css"
+  },
+  {
+    "revision": "f0cae6f5829ea57f0fce",
+    "url": "/static/js/2.cfc48acf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cfc48acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce4b511767afa85ab2d",
+    "url": "/static/js/main.98709b3d.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
