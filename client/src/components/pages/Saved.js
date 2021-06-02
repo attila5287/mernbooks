64da42476bdd03cf1d155e98e2799ccd42230a59
attila5287 <atiturkoz@hotmail.com>
2021-06-02 17:35:05 -0600

    mock api works for savex

--- client/src/components/pages/Saved.js
+++ client/src/components/pages/Saved.js
@@ -13,7 +13,7 @@ const Saved = () => {
 	// Load all books and store them with setBooks
 	useEffect(() => {
 		API.getBooksDB()
-			.then((res) => setSaves(res.data.items))
+			.then((res) => setSaves(res.data))
 			.catch((err) => console.log(err));
   }, [] );
   
@@ -34,6 +34,44 @@ const Saved = () => {
 					<i>Visit book on Google store by using butons </i>
 				</p>
 			</Animated>
+
+			<List>
+				{saves.map((book, idx) => {
+					return (
+						<Animated
+							animationIn='slideInRight'
+							isVisible={true}
+							animationInDelay={idx * 250}
+						>
+							<ListItem key={idx}>
+								<div className='d-flex flex-row justify-content-between bg-secondary p-1 rounded-xl'>
+									<img
+										src={book?.image}
+										alt='thumbnail'
+										className='img-list rounded-lg shadow'
+									/>
+									<a className='nav-link' href={'/api/book/' + book?._id}>
+										<span className='text-capitalize'>
+											{book?.title}
+										</span>{' '}
+										by{' '}
+										<strong>
+											<i className='text-reg text-primary'>
+												{book?.authors
+													? book.authors[0]
+													: ''}
+											</i>
+										</strong>
+									</a>
+									<span className='fa-pull-right'>
+									</span>
+								</div>
+							</ListItem>
+						</Animated>
+					);
+				})}
+			</List>
+
 			<Animated
 				animationIn='bounceInRight'
 				isVisible={true}
